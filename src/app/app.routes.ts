fe6e7700b31ef5d34a568793529ed2ab81970bb7
John Gavin <jgavin2@opentext.com>
2025-02-05 14:55:59 +0000

    Update app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,6 @@ import { HomeComponent } from './home/home.component';
 import { AwayComponent } from './away/away.component';
 
 export const routes: Routes = [
-  { path: 'home', component: HomeComponent },
+  { path: '**', title: 'Hacker News - Home', component: HomeComponent },
   { path: 'away', component: AwayComponent }
 ];
